a99effbd2c07f131696f71d455eb7644a541e290
OhioDschungel6 <OhioDschungel6@users.noreply.github.com>
2022-10-27 10:51:47 +0200

    Upload deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,12 @@ sudo apt-get install git
 git clone https://github.com/OhioDschungel6/NFC_Server.git
 cd NFC_Server
 sudo adduser doorserver gpio
+sudo apt-get install pip
+pip install virtualenv
+virtualenv -p python3 doorEnvironment
+source doorEnvironment/bin/activate
+
+
 cp ./doorserver.service /etc/systemd/system/doorserver.service
 sudo systemctl start doorserver    # Runs the script now
 sudo systemctl enable doorserver #Adds the script as service
