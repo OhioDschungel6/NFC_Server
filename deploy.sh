79747b647e9e5faf49d8f69a66b820806da07de9
OhioDschungel6 <OhioDschungel6@users.noreply.github.com>
2023-01-28 12:38:02 +0100

    Fix deploy file

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ sudo apt-get install python3-rpi.gpio
 sudo adduser doorserver gpio
 sudo apt-get install pip
 pip install virtualenv
-sudo su doorserver
+sudo su doorserver && bash
 
 cd /home/doorserver #change directory
 git clone https://github.com/OhioDschungel6/NFC_Server.git
@@ -17,9 +17,10 @@ source doorEnvironment/bin/activate
 pip install -r requirements.txt
 exit
 
-#Edit config.json to add secretkey
 
 cd /home/doorserver/NFC_Server
 sudo cp ./doorserver.service /etc/systemd/system/doorserver.service
 sudo systemctl start doorserver    # Runs the script now
 sudo systemctl enable doorserver #Adds the script as service
+
+#Change config and restart!
